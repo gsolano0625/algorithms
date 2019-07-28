9fc76e4a0c3427fed8299fbd7f1bb5ac51360f48
Gerardo <gerardo.solano@mapquest.com>
2019-07-28 11:18:27 -0600

    Replace tabs with spaces

--- index.js
+++ index.js
@@ -8,5 +8,3 @@ console.log(`=== array [ 4, -6, 3, -1, 4, 2, 7 ] has ${zeroSum.hasZeroSums([ 4, 
 console.log(`=== array [ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2] has ${zeroSum.hasZeroSums([ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2])} zero sums`);
 console.log(`=== array [ 3, 4, 3, 1, ] has ${zeroSum.hasZeroSums([ 3, 4, 3, 1])} zero sums`);
 
-// TODO 
-console.log(`=== array [ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2] has ${zeroSum.getZeroSums([ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2])}`);
